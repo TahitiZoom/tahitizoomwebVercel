68c5b525d93b15ce9d5932959ebeab87bab6f396
TahitiZoom <contact@tahitizoom.pf>
2026-03-28 04:19:30 +0000

    feat: Tahiti Zoom baseline propre - prod OK

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260327_223153 from './20260327_223153';
+
+export const migrations = [
+  {
+    up: migration_20260327_223153.up,
+    down: migration_20260327_223153.down,
+    name: '20260327_223153'
+  },
+];
