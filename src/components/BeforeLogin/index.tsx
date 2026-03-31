d43a298d95480743f8299b5e58ab62c71585e2c5
TahitiZoom <contact@tahitizoom.pf>
2026-03-31 19:11:22 +0000

    fix: logo login correct logo-tz.png

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -18,7 +18,7 @@ export const BeforeLogin: React.FC = () => {
       />
       {/* Logo signature */}
       <img
-        src="/logo-signature.png"
+        src="/logo.svg"
         alt="Stéphane Sayeb"
         style={{ width: '220px', marginBottom: '1rem' }}
       />
