e6eaca0f09ed2a1e636c91048dbe5043a6752fe5
TahitiZoom <contact@tahitizoom.pf>
2026-04-04 19:54:33 +0000

    chore: clean media collection comments

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -63,7 +63,6 @@ export const Media: CollectionConfig = {
     ],
   },
   upload: {
-    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
     adminThumbnail: 'thumbnail',
     focalPoint: true,
     // Conversion automatique en WebP pour tous les nouveaux uploads
