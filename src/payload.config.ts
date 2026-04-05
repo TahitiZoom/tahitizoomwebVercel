f004582e3ba6f3246900908839e67f1004720d64
TahitiZoom <contact@tahitizoom.pf>
2026-04-05 02:09:11 +0000

    fix: customize payload admin title and favicon

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,6 +37,10 @@ export default buildConfig({
     },
   }),
   admin: {
+    meta: {
+      titleSuffix: ' - Tahiti Zoom',
+      favicon: '/favicon.ico',
+    },
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below.
